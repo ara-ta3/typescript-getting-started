551a6a0d645fae3d68ce5c9efb39ef8545249428
Arata Tanaka <tarata43@yahoo.co.jp>
2018-09-17 23:16:35 +0900

    ...

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,8 @@
-function hoge(): Promise<string> {
-    return new Promise((resolve: Function, reject: Function) => {
-        reject(new Error("hoge"));
-    });
-}
-
-function fuga(): Promise<string> {
-    return new Promise((resolve: Function, reject: Function) => {
-        reject(new Error("fuga"));
-    });
-}
+import * as filesize from "filesize";
 
 async function main() {
-    const a1 = await hoge();
-    const a2 = await fuga();
-    const x = await hoge()
-        .then((v) => console.log(v))
-        .catch((e: Error) => console.error(e)); // catchがないとUnhandledPromiseRejectionWarningが出る
-    console.log(x);
+  const x = filesize(1000);
+  console.log(x);
 }
 
 main();
